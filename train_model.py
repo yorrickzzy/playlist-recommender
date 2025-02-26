e6549629d310a20e0355be9babcae38365a1eedf
yorrickzzy <zhangzy0110@gmail.com>
2025-02-26 06:33:58 -0700

    update

--- train_model.py
+++ train_model.py
@@ -2,7 +2,16 @@ from fpgrowth_py import fpgrowth
 import pandas as pd
 import pickle
 
-df = pd.read_csv("/home/datasets/spotify/2023_spotify_ds2.csv")
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+dataset_path = os.path.join(BASE_DIR, "../dataset/dataset/dataset_v2.csv")
+
+if not os.path.exists(dataset_path):
+    raise FileNotFoundError(f"Dataset file not found: {dataset_path}")
+
+df = pd.read_csv(dataset_path)
+
+#df = pd.read_csv("/home/datasets/spotify/2023_spotify_ds2.csv")
 df['track_name'] = df['track_name'].str.strip().str.lower()  
 df['track_name'] = df['track_name'].str.rstrip('.')
 transactions = df.groupby("pid")["track_name"].apply(list).tolist()
